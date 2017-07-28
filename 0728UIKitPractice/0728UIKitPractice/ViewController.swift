af520fa912d0f5fcd8ed310ca7afe84e945a813e
Tsai-Ting Yang <u9934007@gmail.com>
2017-07-28 09:43:22 +0800

    segmentControl

--- 0728UIKitPractice/0728UIKitPractice/ViewController.swift
+++ 0728UIKitPractice/0728UIKitPractice/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        self.view.backgroundColor = UIColor.white
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        let segmentControl = UISegmentedControl(items: [ "1", "2", "3", "4"])
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        segmentControl.frame = CGRect(x: 50, y: 50, width: 120, height: 30)
+        segmentControl.center.x = CGFloat(screenWidth/2)
+        segmentControl.center.y = CGFloat(screenHeight/4)
 
+        segmentControl.selectedSegmentIndex = 1
+
+//        segmentControl.addTarget(self, action: "changePicture:", for: UIControlEvents.valueChanged)
+
+        self.view.addSubview(segmentControl)
+
+    }
 
 }
-
